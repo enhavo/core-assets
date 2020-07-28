85d2a3a4b6825be42e380ac4df4b76a34b8929f6
Gerhard Seidel <gseidel.message@googlemail.com>
2020-07-28 15:01:37 +0200

    Move files to dependency injection folder

--- DependencyInjection/ServiceLoader.js
+++
@@ -1,9 +0,0 @@
-
-const YamlLoader = require('./YamlLoader');
-const Compiler = require('./Compiler');
-
-module.exports = function(source) {
-    let builder = YamlLoader.load(source);
-    let compiler = new Compiler;
-    return compiler.compile(builder);
-};
